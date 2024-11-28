1516939b85b3086c0414c284ddf22988a0841bdc
EliezerMejia17 <eliezermejia161704@gmail.com>
2024-11-28 11:34:06 -0400

    Agregando Paginas de landing

---
+++ lib/screens/landing/filosofia_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:uasd_app/utils/app_colors.dart';
+
+class FilosafiaScreen extends StatelessWidget {
+  const FilosafiaScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    // Para el tema del texto
+    TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("UASD"),
+      ),
+      body: SingleChildScrollView (
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Text(
+              "Filosofía", 
+              style: textTheme.titleMedium,
+              ),
+            Text(
+              "La Universidad es una institución que une a profesores, estudiantes y  servidores de apoyo a la labor académica, con el propósito de dar cumplimiento a la misión y  la visión universitarias.La Universidad Autónoma de Santo Domingo es un patrimonio social público de alto interés estratégico que forma parte del Sistema Nacional de Educación Superior Estatal, integrada por la Sede Central, Recintos, Centros y Subcentros Universitarios, esparcidos en la geografía del país y en el exterior, con centralización normativa y descentralización operativa e interdependientes, que se gestiona bajo el régimen de autonomía con responsabilidad.\nEl modelo educativo institucional de la Universidad Autónoma de Santo Domingo está sustentado en la docencia, la investigación y la extensión; busca fortalecer el desarrollo de una conciencia crítica, el pensamiento reflexivo y creativo, así como las demás funciones y actividades que se requieren para el cumplimiento de su misión.", 
+              style: textTheme.bodyMedium,
+            ),
+             SizedBox(height: 20),
+             Image.network(
+                'https://listindiario.com/files/main_image/uploads/2021/02/03/64324d9df00f5.jpeg',
+                width: 500,
+                height: 250,
+                fit: BoxFit.cover, // Ajusta cómo se muestra la imagen
+              ), 
+
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/landing/funciones_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:uasd_app/utils/app_colors.dart';
+
+class FuncionesScreen extends StatelessWidget {
+  const FuncionesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    // Para el tema del texto
+    TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("UASD"),
+      ),
+      body: SingleChildScrollView (
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Text(
+              "Funciones", 
+              style: textTheme.titleMedium,
+              ),
+             SizedBox(height: 20),
+             Image.network(
+                'https://www.porlalinea.com.do/wp-content/uploads/2023/02/fachada-uasd-nota-240818-1024x768-1.jpg',
+                width: 500,
+                height: 280,
+                fit: BoxFit.cover, 
+              ),
+              Text(
+              "Para el cumplimiento de su misión, la Universidad tiene varias funciones, las cuales se desarrollan en procesos y actividades diversas. Las funciones principales son, según el Estatuto Orgánico (Arts. 5 y 6), la docencia, la investigación y la extensión.\n Cada una de estas funciones agrupa un conjunto de actividades en torno a una unidad específica y la totalidad de éstas expresan la misión. En síntesis el contenido axial y estructural de las funciones es el siguiente:", 
+              style: textTheme.bodyMedium,
+            ), 
+            Text(
+              "LA DOCENCIA", 
+              style: textTheme.titleSmall,
+            ),
+            Text(
+              "Es la función por la que se trasmite en el proceso académico los conocimientos, se dirige la aplicación de éstos a la realidad espacio/temporal y se desarrolla la actitud heurística. Se cumple a través de la Facultad y sus distintas expresiones (Escuela, Cátedra).Tiene como organismo central la Vicerrectoría Docente.", 
+              style: textTheme.bodyMedium,
+            ),
+            Text(
+              "LA INVESTIGACIÓN", 
+              style: textTheme.titleSmall,
+            ),
+            Text(
+              "Se orienta hacia la búsqueda de conocimientos nuevos y a la comprobación de los ya existentes. Su meta es la superación del acervo existente, el entrenamiento de los estudiantes/ profesionales en los métodos del conocimiento y actuar sobre los problemas sociales prioritarios. Las unidades estructurales encargadas de la política de esta función y las unidades de base a través de las cuales se cumple son las Facultades y los Institutos.", 
+              style: textTheme.bodyMedium,
+            ),
+            Text(
+              "LA ADMINISTRACIÓN", 
+              style: textTheme.titleSmall,
+            ),
+            Text(
+              "Es una función que se sustenta, entre otras, en forma de actividades específicas que sirven de apoyo a todo el proceso institucional. Su órgano central es la Vicerrectoría Administrativa, pero se manifiesta en todas las unidades docentes, docente/administrativas y administrativas.", 
+              style: textTheme.bodyMedium,
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
